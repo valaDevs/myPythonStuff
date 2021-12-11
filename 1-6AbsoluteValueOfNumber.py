98910cc45f1bc530986b05bc9aebfd09daf84c14
vala <vala77fitos@gmail.com>
2021-12-11 13:07:39 -0500

    super simple python code examples

---
+++ 1-6AbsoluteValueOfNumber.py
@@ -0,0 +1,8 @@
+from types import UnionType
+
+
+num = int(input("Enter a negative number : "))
+if num < 0:
+    num = -num
+    
+print("The absolutr value is : ", num)
---
+++ 1-7ODDorEvenn.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the number to check if it is odd or even : "))
+
+if num % 2 == 0:
+    print("EVEN")
+else:
+    print("ODD")
+
+
+    
